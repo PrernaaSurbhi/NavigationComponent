ef0835376d7db5292c73869683485215d743745e
PrernaaSurbhi <prerna.surbhi@gmail.com>
2022-01-30 18:01:04 +0530

    Starting project for navigation component

---
+++ app/src/main/java/com/example/myapplication/TitleFragment.kt
@@ -0,0 +1,22 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.myapplication.databinding.TitleFragmentBinding
+
+/**
+ * Created by PrernaSurbhi on 29/01/22.
+ */
+class TitleFragment: Fragment() {
+    lateinit var binding: TitleFragmentBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater,R.layout.title_fragment,container,false)
+        return binding.root
+    }
+}
