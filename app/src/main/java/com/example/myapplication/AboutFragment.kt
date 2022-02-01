29098fc776d5fc397af2fd7425e6ea62366cbdc7
Prerna <>
2022-02-01 17:14:40 +0530

    Added conditional logic on game frament Added navigation between gameOverFragmnet and gameWonFragment added attribute popUpTo , for explicitly add the condition for back pressure

--- app/src/main/java/com/example/myapplication/AboutFragment.kt
+++ app/src/main/java/com/example/myapplication/AboutFragment.kt
@@ -5,20 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
-import com.example.myapplication.databinding.GameOverFramentBinding
 
 
-class gameOverFrament : Fragment() {
-    lateinit var binding: GameOverFramentBinding
+class AboutFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding = DataBindingUtil.inflate(inflater,R.layout.game_over_frament,container,false)
-        return binding.root
+        return inflater.inflate(R.layout.fragment_about, container, false)
     }
 
 
--- app/src/main/java/com/example/myapplication/gameWonFragment.kt
+++ app/src/main/java/com/example/myapplication/gameWonFragment.kt
@@ -5,20 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.myapplication.databinding.FragmentGameWonBinding
+import com.example.myapplication.databinding.GameOverFramentBinding
 
 class gameWonFragment : Fragment() {
+   lateinit var binding:FragmentGameWonBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
-
-    override fun onCreateView(
+   override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_game_won, container, false)
+       binding = DataBindingUtil.inflate(inflater,R.layout.fragment_game_won,container,false)
+        return binding.root
     }
 
 }
